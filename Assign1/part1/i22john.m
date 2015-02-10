52d7391708e9cc7b421ec57acf478b138143bef4
Kasper Passov <pvx884@alumni.ku.dk>
2015-02-10 15:53:17 +0100

    test and trainer files

--- Assign1/part1/i22john.m
+++ Assign1/part1/i22john.m
@@ -10,27 +10,3 @@ figure(1)
 scatter(z(1,:),z(2,:))
 figure(2)
 scatter(y(1,:),y(2,:))
-
-figure(3)
-hold on
-
-scatter(y(1,:),y(2,:),'cyan')
-
-my1 = mean(y(1,:))
-my2 = mean(y(2,:))
-scatter(my1,my2,72,'blue','+')
-scatter(1,2,72,'red','+')
-
-hold off
-
-mu = [my1 ; my2];
-
-ynew = bsxfun(@minus,y,mu) * bsxfun(@minus,y,mu)';
-ycov = ynew / 100;
-
-[v,d] = eig(ycov);
-
-mu + sqrt(d(1,1)) * v(:,1)
-mu + sqrt(d(2,2)) * v(:,2)
-
-
