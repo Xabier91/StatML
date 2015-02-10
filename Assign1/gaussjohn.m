d4330fe29fb3e60fe0b628abf3c737bf413992eb
Kasper Passov <pvx884@alumni.ku.dk>
2015-02-10 13:43:20 +0100

    removed dublicate and you will never guess what i did to the report

--- Assign1/gaussjohn.m
+++
@@ -1,8 +0,0 @@
-function [ f ] = gaussjohn( mu, sigma )
-%GAUSSJOHN Summary of this function goes here
-%   Detailed explanation goes here
-    a = 1 / (sigma * sqrt(2 * pi));
-    f = @(x) a * exp(-((x - mu)^2) / (2 * sigma^2));
-
-end
-
--- Assign1/part1/i22john.m
+++ Assign1/part1/i22john.m
@@ -4,10 +4,8 @@ cov = [0.3, 0.2 ; 0.2, 0.2];
 L = chol(cov,'lower')
 z = randn(2,100);
 
-
 y = bsxfun(@plus,mu,L * z);
-
 
 scatter(z(1,:),z(2,:))
 figure()
-scatter(y(1,:),y(2,:))matlab.mat
+scatter(y(1,:),y(2,:))
