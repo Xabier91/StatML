005ecb92bce25a131705c5720ba8212428ef6ec7
dread-muffin <dreadmuffin@gmail.com>
2015-02-10 13:24:01 +0100

    i22john

---
+++ Assign1/part1/i22john.m
@@ -0,0 +1,13 @@
+mu = [1;2];
+cov = [0.3, 0.2 ; 0.2, 0.2];
+
+L = chol(cov,'lower')
+z = randn(2,100);
+
+
+y = bsxfun(@plus,mu,L * z);
+
+
+scatter(z(1,:),z(2,:))
+figure()
+scatter(y(1,:),y(2,:))matlab.mat
