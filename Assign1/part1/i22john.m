49157197a72cfd1c74b779f59169858556103cdc
dread-muffin <dreadmuffin@gmail.com>
2015-02-12 15:37:09 +0100

    merge inc

--- Assign1/part1/i22john.m
+++ Assign1/part1/i22john.m
@@ -4,7 +4,7 @@ cov = [0.3, 0.2 ; 0.2, 0.2];
 L = chol(cov,'lower');
 z = randn(2,100);
 
-y = bsxfun(@plus,mu,L * z)
+y = bsxfun(@plus,mu,L * z);
 
 figure(1)
 scatter(z(1,:),z(2,:))
@@ -12,16 +12,15 @@ figure(2)
 scatter(y(1,:),y(2,:))
 
 figure(3)
+clf;
 hold on
 
 scatter(y(1,:),y(2,:),'cyan')
 
-my1 = mean(y(1,:))
-my2 = mean(y(2,:))
+my1 = mean(y(1,:));
+my2 = mean(y(2,:));
 scatter(my1,my2,72,'blue','+')
 scatter(1,2,72,'red','+')
-
-hold off
 
 mu = [my1 ; my2];
 
@@ -30,7 +29,15 @@ ycov = ynew / 100;
 
 [v,d] = eig(ycov);
 
-mu + sqrt(d(1,1)) * v(:,1)
-mu + sqrt(d(2,2)) * v(:,2)
+sv1 = mu + sqrt(d(1,1)) * v(:,1);
+sv2 = mu + sqrt(d(2,2)) * v(:,2);
 
 
+
+quiver(mu(1,1),mu(2,1),sv1(1,1),sv1(2,1));
+quiver(mu(1,1),mu(2,1),sv2(1,1),sv2(2,1));
+
+%plot(sv1)
+%plot(sv2)
+
+hold off
