93f53cad5bd9ba3dee59bfad6dd68e96671e5117
dread-muffin <dreadmuffin@gmail.com>
2015-02-17 14:24:15 +0100

    part1

--- Assign1/part1/i22john.m
+++ Assign1/part1/i22john.m
@@ -1,58 +1,52 @@
+%I.2.2
 mu = [1;2];
 cov = [0.3, 0.2 ; 0.2, 0.2];
 
 L = chol(cov,'lower');
 z = randn(2,100);
-
 y = bsxfun(@plus,mu,L * z);
 
 figure(1)
-scatter(z(1,:),z(2,:))
+p1 = scatter(z(1,:),z(2,:));
 figure(2)
-scatter(y(1,:),y(2,:))
+p2 = scatter(y(1,:),y(2,:));
 
+saveas(p1, 'I221.png','png');
+saveas(p2, 'I222.png','png');
+
+
+%I.2.3
 figure(3)
 clf;
 hold on
 axis equal;
 
-scatter(y(1,:),y(2,:),'cyan')
+p3 = scatter(y(1,:),y(2,:),'cyan');
 
 my1 = mean(y(1,:));
 my2 = mean(y(2,:));
-scatter(my1,my2,72,'blue','+')
-scatter(1,2,72,'red','+')
+scatter(my1,my2,72,'blue','+');
+scatter(1,2,72,'red','+');
 
-mu = [my1 ; my2];
+mu = [my1 ; my2]
+
+% I.2.4
 
 ynew = bsxfun(@minus,y,mu) * bsxfun(@minus,y,mu)';
 ycov = ynew / 100;
 
 [v,d] = eig(ycov);
 
-
-
-%sv1 = mu + sqrt(d(1,1)) * v(:,1);
-%sv2 = mu + sqrt(d(2,2)) * v(:,2);
-
 v1 = sqrt(d(1,1)) * v(:,1);
 v2 = sqrt(d(2,2)) * v(:,2);
 
+quiver(my1,my2,v1(1,1),v1(2,1));
+quiver(my1,my2,v2(1,1),v2(2,1));
 
-
-
-quiver(1,2,v1(1,1),v1(2,1));
-quiver(1,2,v2(1,1),v2(2,1));
-
-%quiver(mu(1,1),mu(2,1),sv1(1,1),sv1(2,1));
-%quiver(mu(1,1),mu(2,1),sv2(1,1),sv2(2,1));
-
-%plot(sv1)
-%plot(sv2)
-
+saveas(p3, 'I231.png','png');
 hold off
 
-% I.2.4
+
 rm = rotationm(30);
 rcov30 = rm^-1 * ycov * rm;
 
@@ -71,27 +65,22 @@ figure(4)
 clf;
 hold on
 
-rm = rotationm(40)
-rcovnew = rm^-1 * ycov * rm;
-ynew = bsxfun(@plus,mu,rcovnew * z);
-
-scatter(y30(1,:),y30(2,:),'yellow')
+p4 = scatter(y30(1,:),y30(2,:),'yellow');
 scatter(y60(1,:),y60(2,:),'blue')
 scatter(y90(1,:),y90(2,:),'red')
-scatter(ynew(1,:),ynew(2,:),'green')
 
 figure(5)
 clf;
-rm = rotationm(40)
+hold on
+
+rm = rotationm(40);
+
 rcovnew = rm^-1 * ycov * rm;
 ynew = bsxfun(@plus,mu,rcovnew * z);
-
-
-scatter(ynew(1,:),ynew(2,:),'blue')
-rm = rotationm(45)
-rcovnew = rm^-1 * ycov * rm;
-ynew = bsxfun(@plus,mu,rcovnew * z);
-
-
-scatter(ynew(1,:),ynew(2,:),'green')
+p5 = scatter(ynew(1,:),ynew(2,:),'green');
 axis equal
+
+saveas(p4, 'I241.png','png');
+saveas(p5, 'I242.png','png');
+
+hold off
